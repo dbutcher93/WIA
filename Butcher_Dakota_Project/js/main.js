0735bf09c4cc354f771a87295b9893d75e2ceb29
dbutcher93 <dbutcher93@fullsail.edu>
2014-09-25 23:39:49 -0400

    4

---
+++ Butcher_Dakota_Project/js/main.js
@@ -0,0 +1,11 @@
+$(function() { 
+		
+		$("#show").click(function(){
+			$("#usa").animate({width:"720px"}, 1000);
+			
+			$("#international").animate({width:"240px"}, 1000);
+			
+			});
+		
+		
+});
